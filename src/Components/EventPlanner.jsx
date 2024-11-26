45dbb128188cecd264921e4227246cb3d9616958
Graeme Bargen <77522859+GraemeBargen@users.noreply.github.com>
2024-11-26 00:53:21 +0000

    Added footer to EventPlanner object

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from './Footer';
 import './EventPlanner.css'; // Import CSS file for styling
 
 const EventPlanner = () => {
@@ -75,6 +76,7 @@ const EventPlanner = () => {
                     <button className='submit-button'>Send</button>
                 </form>
             </section>
+            <Footer/>
         </div>
     );
 };
